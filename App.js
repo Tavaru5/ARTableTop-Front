6115b7960a0d507c338d8c8f78f3162ea0e67087
Silas Brumwell <silas.brumwell@autozone.com>
2018-09-11 20:56:11 -0500

    Fixed key stuff and renamed HelloWorld

--- App.js
+++ App.js
@@ -18,14 +18,14 @@ import {
 } from "react-native";
 
 import { ViroARSceneNavigator } from "react-viro";
+import keys from "./.keys.js";
 
 var sharedProps = {
-  apiKey: "04BFD613-7795-49AF-99BD-C65F22308612"
+  apiKey: keys.AppKey
 };
 
-// Sets the default scene you want for AR and VR
-var InitialARScene = require("./js/HelloWorldSceneAR");
-var InitialVRScene = require("./js/HelloWorldScene");
+// Sets the default scene
+var Game = require("./js/Game");
 
 var AR_NAVIGATOR_TYPE = "AR";
 
@@ -38,71 +38,14 @@ export default class ARTableTop extends Component {
     };
   }
 
-  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
-  // if you are building a specific type of experience.
   render() {
     return (
       <ViroARSceneNavigator
         {...this.state.sharedProps}
-        initialScene={{ scene: InitialARScene }}
+        initialScene={{ scene: Game }}
       />
     );
   }
 }
-
-var localStyles = StyleSheet.create({
-  viroContainer: {
-    flex: 1,
-    backgroundColor: "black"
-  },
-  outer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "black"
-  },
-  inner: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    backgroundColor: "black"
-  },
-  titleText: {
-    paddingTop: 30,
-    paddingBottom: 20,
-    color: "#fff",
-    textAlign: "center",
-    fontSize: 25
-  },
-  buttonText: {
-    color: "#fff",
-    textAlign: "center",
-    fontSize: 20
-  },
-  buttons: {
-    height: 80,
-    width: 150,
-    paddingTop: 20,
-    paddingBottom: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: "#68a0cf",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#fff"
-  },
-  exitButton: {
-    height: 50,
-    width: 100,
-    paddingTop: 10,
-    paddingBottom: 10,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: "#68a0cf",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#fff"
-  }
-});
 
 module.exports = ARTableTop;
